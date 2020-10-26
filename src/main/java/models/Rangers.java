328fa786fb5dfc56cb1034440e5bcd27cc4a3fc4
Ashisoma <okalionesmu@gmail.com>
2020-10-26 21:15:28 +0300

    create ranger class

---
+++ src/main/java/models/Rangers.java
@@ -0,0 +1,25 @@
+package models;
+
+public class Rangers {
+    private String rangerName;
+    private int contact;
+    private int badgeNumber;
+
+    public Rangers(String rangerName, int contact, int badgeNumber) {
+        this.rangerName = rangerName;
+        this.contact = contact;
+        this.badgeNumber = badgeNumber;
+    }
+
+    public String getRangerName() {
+        return rangerName;
+    }
+
+    public int getContact() {
+        return contact;
+    }
+
+    public int getBadgeNumber() {
+        return badgeNumber;
+    }
+}
